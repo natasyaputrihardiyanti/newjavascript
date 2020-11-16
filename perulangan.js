422dae882baf723e552658887d4198d3ffb3d47c
natasyaputrihardiyanti <natasya_putri_27rpl@student.smktelkom-mlg.sch.id>
2020-11-16 13:37:33 +0700

    Praktikum Java

---
+++ perulangan.js
@@ -0,0 +1,40 @@
+//FOR LOOP
+// for (let index = 1; index <= 10; index++){
+//     console.log("Perulangan ke- " + index)
+// }
+
+
+//FOR/IN LOOP
+// let siswa = {
+//     nama : "Lathifinda Rosari",
+//     gender : "Wanita",
+//     jurusan: "RPL",
+//     usia: "18",
+//     alamat: "Los Angles"
+// }
+
+// for (key in siswa) {
+//     console.log(key);
+// }
+
+//FOR/ OF LOOP
+// let presiden = ["Soekaro", "Habibi", "Gus Dur", "Megawati", "SBY", "Jokowi"]
+// for (pres of presiden) {
+//     console.log(pres)
+// }
+
+//WHILE LOOP
+// let laptop = ["lenovo", "HP", "ACER", "ASUS"]
+// let i = 0
+// while(laptop[i]){
+//     console.log(laptop[i])
+//     i++
+// }
+
+//DO WHILE
+let gadget = ["xioami", "samsung", "motorola", "sony"]
+let i = 0
+do{
+    console.log(gadget[i])
+    i++
+} while(gadget[i])
